4144b6dbe8f4f6e3f952b3cb7930c19068c89d57
UNFlappable <harunyoney@gmail.com>
2024-04-12 06:42:24 +0300

    checkout project starter

---
+++ checkout-axios/src/App.js
@@ -0,0 +1,13 @@
+//? Component
+function App() {
+  //? JS kodu
+
+  return (
+    //! JSX (HTML- JS'nin birleşmiş hali)
+    <div className="App">
+      <h2 style={{ color: "red" }}>Hoşgeldin Ya Şehri React</h2>
+    </div>
+  );
+}
+
+export default App;
