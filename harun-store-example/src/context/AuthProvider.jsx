49550b0a50d5e84698c34d36caf5421e570ff679
UNFlappable <harunyoney@gmail.com>
2024-04-22 20:25:51 +0300

    proje-devam

--- harun-store-example/src/context/AuthProvider.jsx
+++ harun-store-example/src/context/AuthProvider.jsx
@@ -1,19 +1,23 @@
-import React, { createContext } from 'react'
-const AuthContext= createContext()
+import React, { Children, createContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
+// const {Provider} =  createContext()
+export const AuthContext = createContext();
 
-const AuthProvider = ({children}) => {
-    const [user, setUser] = useState()
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState();
+  const navigate = useNavigate();
+
+  const login = (info) => {
+    setUser(info);
+    navigate("/home");
+  };
 
   return (
-    <AuthContext.Provider value={user}>
-        {children}
-
-
+    <AuthContext.Provider value={{ user, login }}>
+      {children}
     </AuthContext.Provider>
+  );
+};
 
-    
-  )
-}
-
-export default AuthProvider
+export default AuthProvider;
--- harun-store-example/src/router/AppRouter.jsx
+++ harun-store-example/src/router/AppRouter.jsx
@@ -5,18 +5,28 @@ import Home from "../pages/Home";
 import Products from "../pages/Products";
 import About from "../pages/About";
 import NotFound from "../pages/NotFound";
+import Navbar from "../components/Navbar";
+import PrivateRouter from "./PrivateRouter";
 
 const AppRouter = () => {
   return (
-    <BrowserRouter>
+    // <BrowserRouter>
+    <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<PrivateRouter />}>
+          <Route path="" element={<Home />} />
+          <Route path="/dashboard/products" element={<Products />} />
+          {/* //* absolute path */}
+          <Route path="/dashboard/about" element={<About />} />
+          {/* //* relative path */}
+          {/* <Route path="about" element={<About />} /> */}
+        </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </BrowserRouter>
+    </>
+    // </BrowserRouter>
   );
 };
 
