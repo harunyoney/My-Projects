4d31c3088608a499353e432eeb68cebbb0ea4811
UNFlappable <harunyoney@gmail.com>
2024-04-27 17:59:02 +0300

    firebase start

--- mui-starter/src/components/TypoButtons.jsx
+++ mui-starter/src/components/TypoButtons.jsx
@@ -1,5 +1,15 @@
+import Typography from '@mui/material/Typography';
+
 const TypoButtons = () => {
-  return <div>TypoButtons</div>;
+  return <div>
+    
+      <Typography>TypoButtons</Typography>
+      <Typography variant='body1' color="violet" >KEKW 5248877 ASDE</Typography>
+   <Typography variant='subtitle1' color="primary" mt={2}>KEKW 5248877 ASDE</Typography>
+
+<Typography variant='button' color="secondary" mt={2}>KEKW 5248877 ASDE</Typography>
+
+      </div>;
 };
 
 export default TypoButtons;
--- mui-starter/src/pages/Home.jsx
+++ mui-starter/src/pages/Home.jsx
@@ -1,4 +1,9 @@
 import TypoButtons from "../components/TypoButtons";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 const Home = () => {
   return (
