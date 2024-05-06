70b0e2c3bf9620ebb715a0c0137375418c868341
UNFlappable <harunyoney@gmail.com>
2024-05-06 22:33:17 +0300

    pages

--- stock-app-BIG/src/router/AppRouter.jsx
+++ stock-app-BIG/src/router/AppRouter.jsx
@@ -1,9 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
-import Login from "../pages/Login";
-import Register from "../pages/Register";
-import PrivateRouter from "./PrivateRouter";
-import Dashboard from "../pages/Dashboard";
+import Login from "../pages/Login"
+import Register from "../pages/Register"
+import PrivateRouter from "./PrivateRouter"
+import Dashboard from "../pages/Dashboard"
+import Home from "../pages/Home"
+import Sales from "../pages/Sales"
 
 const AppRouter = () => {
   return (
@@ -12,11 +14,14 @@ const AppRouter = () => {
         <Route path="/" element={<Login />} />
         <Route path="register" element={<Register />} />
         <Route path="stock" element={<PrivateRouter />}>
-          <Route path="" element={<Dashboard />} />
+          <Route path="" element={<Dashboard />}>
+            <Route index element={<Home />} />
+            <Route path="sales" element={<Sales />} />
+          </Route>
         </Route>
       </Routes>
     </Router>
-  );
-};
+  )
+}
 
-export default AppRouter;
+export default AppRouter
