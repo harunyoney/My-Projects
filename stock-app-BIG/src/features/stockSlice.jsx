302028ff8b91e8354805fea333d7f497a86d1ed2
UNFlappable <harunyoney@gmail.com>
2024-05-10 21:52:12 +0300

    push

--- stock-app-BIG/src/features/stockSlice.jsx
+++ stock-app-BIG/src/features/stockSlice.jsx
@@ -1,55 +1,30 @@
-import { createSlice } from "@reduxjs/toolkit"
-
-const initialState = {
-  purchases: [],
-  sales: [],
-  firms: [],
-  products: [],
-  brands: [],
-  categories: [],
-  loading: false,
-  error: false,
+import { createSlice } from "@reduxjs/toolkit";
+const initialState={
+   purchases:[],
+   sales:[],
+   firms:[],
+   products:[],
+   brands:[],
+   categories:[],
+   loading:false,
+   error:false,
 }
 
-const stockSlice = createSlice({
-  name: "stock",
-  initialState,
-  reducers: {
-    fetchStart: (state) => {
-      state.loading = true
-    },
-    // getFirmsSuccess: (state, { payload }) => {
-    //   state.loading = false
-    //   state.firms = payload
-    // },
-    // getSalesSuccess: (state, { payload }) => {
-    //   state.loading = false
-    //   state.sales = payload
-    // },
+const stockSlice=createSlice({
+    name:"",
+    initialState:initialState,
+    reducers:{
+        fetchStart:(state)=>{
+            state.loading=true
+        },
+        fetchSuccess:(state,{payload})=>{
+            state.loading=false,
 
-    //? action.payload.path
-    // getStockSuccess: (state, action) => {
-    //   state.loading = false
-    //   state[action.payload.path] = action.payload.stockData
-    // },
+        },
+        fetchFail:(state)=>{
+            state.loading=false,
+            state.error=true
+        },
 
-    getStockSuccess: (state, { payload: { path, stockData } }) => {
-      state.loading = false
-      state[path] = stockData
     },
-    fetchFail: (state) => {
-      state.loading = false
-      state.error = true
-    },
-  },
 })
-
-export const {
-  fetchStart,
-  getFirmsSuccess,
-  getSalesSuccess,
-  getStockSuccess,
-  fetchFail,
-} = stockSlice.actions
-
-export default stockSlice.reducer
