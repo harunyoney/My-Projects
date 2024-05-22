b03bc8d01f2b354766ca11781cb4c392f24a3266
UNFlappable <harunyoney@gmail.com>
2024-05-22 21:40:04 +0300

    ts

---
+++ TypeScript/first-day/out/app.js
@@ -0,0 +1,8 @@
+console.log("hello world");
+let twq = 12;
+const topla = (a, b) => { a + b; };
+let a = 5;
+let num = [1, 2, 3];
+let num2 = ["kmasd", "owoe"];
+num.push(3);
+num2.push(3);
---
+++ TypeScript/first-day/src/app.ts
@@ -0,0 +1,12 @@
+console.log("hello world");
+let twq=12   //qwe
+// kamsde
+
+const topla=(a,b)=>{a+b}
+
+let a :number=5
+
+let num: number[]=[1,2,3]
+let num2 :Array<string>=["kmasd","owoe"]
+num.push(3)
+num2.push("3")
